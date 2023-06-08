20d06dd06c281901b706105ee8eab3756ed025ad
AnBarr45 <bpena2063@e-camp.cl>
2023-06-07 21:40:43 -0400

    lista_rental completo

--- comandos/listar_rental.sql
+++ comandos/listar_rental.sql
@@ -1,1 +1,14 @@
 SELECT 
+customer.store_id,
+customer.first_name,
+customer.last_name, 
+customer.email, 
+customer.active,
+rental.rental_date,
+rental.rental_id
+FROM rental JOIN customer 
+-- ON rental.customer_id = customer.customer.id
+ON rental.customer_id = customer.customer_id
+WHERE EXTRACT(YEAR FROM rental_date) = 2005
+AND EXTRACT(MONTH FROM rental_date) = 5
+GROUP BY rental_date,rental_id, store_id, first_name, last_name,email,active;
