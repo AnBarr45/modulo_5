6f4f3812dd6db95b7ced4a4c22c0344c915c2c95
AnBarr45 <bpena2063@e-camp.cl>
2023-06-08 16:30:51 -0400

    payments completa

--- comandos/payments.sql
+++ comandos/payments.sql
@@ -1,8 +1,7 @@
 SELECT 
-  payment_id, 
-  payment_date, 
-  -- SUM(amount)
-  amount
+  (SELECT COUNT(DISTINCT payment_id) FROM payment) AS numero_ids,
+  (SELECT COUNT(DISTINCT payment_date) FROM payment) AS numero_fechas,  
+  (SELECT SUM(amount) FROM payment)
 FROM  payment
-
-GROUP BY payment_date,payment_id,amount;
+GROUP BY payment_date,payment_id,amount
+LIMIT 1;
