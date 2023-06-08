4c641b116215c917e3ed78489e65bdf26c93827d
AnBarr45 <bpena2063@e-camp.cl>
2023-06-07 21:46:00 -0400

    limpiar comentarios

--- comandos/listar_rental.sql
+++ comandos/listar_rental.sql
@@ -7,8 +7,6 @@ customer.email,
 customer.store_id,
 customer.active
 FROM rental JOIN customer 
--- ON rental.customer_id = customer.customer.id
 ON rental.customer_id = customer.customer_id
 WHERE EXTRACT(YEAR FROM rental_date) = 2005
 AND EXTRACT(MONTH FROM rental_date) = 5
--- GROUP BY rental_date,rental_id, store_id, first_name, last_name,email,active;
